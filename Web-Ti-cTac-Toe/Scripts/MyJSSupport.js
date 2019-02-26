6a61ec198f7598802edca52860a3b953a8320516
Юлий Малков <malkov.1988@inbox.ru>
2019-02-27 02:50:41 +0800

    fail code, save just in case

--- Web-Ti-cTac-Toe/Scripts/MyJSSupport.js
+++ Web-Ti-cTac-Toe/Scripts/MyJSSupport.js
@@ -1,14 +1,32 @@
-﻿function Cross(arg) {    
-    $(arg).attr("disabled","disabled");
-    $(arg).children(0).attr("src", '../Content/site/cross.png');
-    Zero(arg);
-}
-
-function AskName() {
+﻿function AskName() {
     var namePlayer = prompt("Введите своё имя");
-    $("h3").get(0).prepend(namePlayer +", ");
+    $("h3").get(0).prepend(namePlayer + ", ");    
 }
 
-function Zero(arg) {    
-    //TODO: some metod click Circle 
+function Cross(arg) {
+    $(arg).attr("disabled", "disabled");
+    $(arg).children(0).attr("src", "../Content/site/cross.png");
+    //CheckWin();
+    Zero();
+}
+
+function Zero() {  
+    //for (var i = 0; i < 9; i++) {
+    var tempVar = $("button.button[disabled='false']").get(0);
+    var temp2 = $(tempVar).children(0);
+    alert($(temp2));
+            if ($(tempVar).html() == "<img src='~/Content/site/emptyPic.png' style='width:30px; height:30px' />")
+            {               
+                alert("WiN");                   
+                $(tempVar).children(0).attr("src", "../Content/site/circle.png");
+                $(tempVar).attr("disabled", "disabled");
+                //CheckWin();
+                return;
+            }
+            else alert("Something wrong");
+       //}
+}
+
+function CheckWin() {
+    //TODO: code this block, checking conditions win
 }
