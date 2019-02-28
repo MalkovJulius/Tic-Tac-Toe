51ac154e177b145a20cf5822c9ef147b343dc267
Юлий Малков <malkov.1988@inbox.ru>
2019-03-01 01:52:05 +0800

    upgrade

--- Web-Ti-cTac-Toe/Scripts/MyJSSupport.js
+++ Web-Ti-cTac-Toe/Scripts/MyJSSupport.js
@@ -1,20 +1,27 @@
-﻿function AskName() {
+﻿//as the page loads asks the player's name
+function AskName()
+{
     var namePlayer = prompt("Введите своё имя");
-    $("h3").get(0).prepend(namePlayer + ", ");    
+    $("h3").get(0).prepend(namePlayer + ", ");   
+    //TODO: set name in DB
 }
 
-function Cross(arg) {
+//puts the cross on the click
+function Cross(arg)
+{
     $(arg).attr("disabled", "disabled");
     $(arg).children(0).attr("src", "../Content/site/cross.png");
     //CheckWin();
-    setTimeout(Zero(),500);   
+    setTimeout(Zero,700);   
 }
 
-function Zero() {      
-    for (var i = 0; i < 9; i++)
-    {
-        var tempVar = $(".button").get(i); 
-        if (!($(tempVar).prop("disabled"))) {  
+//computer turn
+function Zero()
+{      
+    for (var i = 0; i < 9; i++) {
+        var tempVar = $(".buttonGame").get(i); 
+        if (!($(tempVar).prop("disabled")))
+        {  
             $(tempVar).children(0).attr("src", "../Content/site/circle.png");
             $(tempVar).attr("disabled", "disabled");
             //CheckWin();
@@ -23,6 +30,12 @@ function Zero() {
     }    
 }
 
+//Checks whether won
 function CheckWin() {
     //TODO: code this block, checking conditions win
+    if (true)
+        alert("Поздравляю с победой над ИИ");
+    else
+        alert("В этот раз, Вы проиграли, попробуйте ещё раз");
+    //set data in BD    
 }
