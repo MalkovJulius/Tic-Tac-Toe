cb8530333a60c42570edd09da0b04f4fbe34dc94
Юлий Малков <malkov.1988@inbox.ru>
2019-03-20 17:07:03 +0800

    understood how to transmit data

--- Web-Ti-cTac-Toe/Controllers/HomeController.cs
+++ Web-Ti-cTac-Toe/Controllers/HomeController.cs
@@ -5,71 +5,40 @@ using System.Web.UI.WebControls;
 using Web_Tic_Tac_Toe.Models;
 
 namespace Web_Tic_Tac_Toe.Controllers
-{   
+{
     public class HomeController : Controller
     {
         GameContext db = new GameContext();
-              
+
         public ActionResult Index()
-        {            
-            return View(db.Games);
-        }          
-               
-        //public static MvcHtmlString BtnAddClick()
-        //{
-        //    string cross = "~/Content/site/cross.png";            
-        //    string circle = "~/Content/site/circle.png";            
-        //    TagBuilder img = new TagBuilder("img");            
-        //    img.MergeAttribute("src", cross);
-        //    img.MergeAttribute("height", "30");
-        //    img.MergeAttribute("width", "30");
-        //    return MvcHtmlString.Create(img.ToString(TagRenderMode.SelfClosing));
-        //} 
-
-        //public static MvcHtmlString Image(int i, int j)             
-        //{
-        //    string str = "~/Content/site/null.png";
-        //    TagBuilder img = new TagBuilder("img");
-        //    string tempName = i.ToString()+j.ToString();
-        //    img.MergeAttribute("name", tempName);
-        //    img.MergeAttribute("src", str);
-        //    img.MergeAttribute("height", "30");
-        //    img.MergeAttribute("width","30");            
-        //    return MvcHtmlString.Create(img.ToString(TagRenderMode.SelfClosing));
-        //}
-
-        public class EditDb : DbContext
         {
-            public static void RemoveTable(GameContext context)
-            {
-                using (GameContext db = new GameContext())
-                {
-                    context.Games.RemoveRange(context.Games);
-                    db.SaveChanges();
-                }
-            }            
+            return View(db.Games);
         }
 
-        /*public void ButtonClick(ref Button btn)
+        //[System.Web.Services.WebMethod()]
+        //[System.Web.Script.Services.ScriptMethod()]
+        public ActionResult RemoveTable()
         {
-            //Button btn = (Button)sender;
-            btn.Enabled = false;
-            btn.Text = "BOOM";
-        }*/
-
-        /*public void AddCharacter()
-        {
-            int i = 1;
-            int j = 1;
-            string cross = "Content/site/cross.png";
-            string circle = "Content/site/circle.png";
-            string tempName = i.ToString() + j.ToString();
-            if ()
+            //TODO: Fix it, always remove data id Data Base
+            using (GameContext db = new GameContext())
             {
-
+                //context.Games.RemoveRange(context.Games);                    
+                db.Games.RemoveRange(db.Games);
+                db.SaveChanges();
             }
+            //return View(db.Games);
+            //return RedirectToAction("Index");
+            return View("Index");
+        }
 
-        }*/
+        public ActionResult AddData()
+        {
+            using (GameContext db = new GameContext())
+            {
+                db.Games.Add(new Game() { YouWin=true, MoveGame="Set new Value" });
+                db.SaveChanges();
+            }
+            return View("Index");
+        }
     }
-
 }
--- Web-Ti-cTac-Toe/Scripts/MyJSSupport.js
+++ Web-Ti-cTac-Toe/Scripts/MyJSSupport.js
@@ -1,7 +1,7 @@
 ﻿//as the page loads asks the player's name
 function AskName() {
-    var namePlayer = prompt("Введите своё имя");
-    $("h3").get(0).prepend(namePlayer + ", ");   
+    var namePlayer = prompt("Введите своё имя", "anonymous");
+    $("h3").get(0).prepend(namePlayer + ", ");
     //TODO: set name in DB
 }
 
@@ -9,75 +9,81 @@ function AskName() {
 function Cross(arg) {
     $(arg).attr("disabled", "disabled");
     $(arg).children(0).attr("src", "../Content/site/cross.png");
-    $(arg).children(0).attr("alt","cross");
-    CheckWin(arg);    
-    setTimeout(Zero, 300);   
+    $(arg).children(0).attr("alt", "cross");
+    CheckWin(arg);
+    setTimeout(Zero, 300);
 }
 
 //computer turn
-function Zero() {      
+function Zero() {
     for (var i = 0; i < 9; i++) {
-    var targetPC = $(".buttonGame").get(i); 
-        if (!($(targetPC).prop("disabled")))
-        {  
+        var targetPC = $(".buttonGame").get(i);
+        if (!($(targetPC).prop("disabled"))) {
             $(targetPC).children(0).attr("src", "../Content/site/circle.png");
             $(targetPC).children(0).attr("alt", "circle");
             $(targetPC).attr("disabled", "disabled");
             CheckWin(targetPC);
             return;
-        }      
-    } 
+        }
+    }
 }
 
 //Checks whether won
-function CheckWin(someArg) {    
-    var valueProp = $(someArg).children(0).prop("alt");   
-    var valueID = $(someArg).prop("id");    
-    var countEqualProp = 0;   
-    for (var i = 0; i < 3; i++) {        
+function CheckWin(someArg) {
+    var valueProp = $(someArg).children(0).prop("alt");
+    var valueID = $(someArg).prop("id");
+    var countEqualProp = 0;
+    for (var i = 0; i < 3; i++) {
         if (valueProp == $("#" + valueID[0] + i).children(0).prop("alt")) {
             countEqualProp++;
             if (countEqualProp == 3) {
                 ShowWinMessage(valueProp)
             }
         }
-        else break;        
+        else break;
     }
     countEqualProp = 0;
-    for (var i = 0; i < 3; i++) {        
+    for (var i = 0; i < 3; i++) {
         if (valueProp == $("#" + i + valueID[1]).children(0).prop("alt")) {
             countEqualProp++;
             if (countEqualProp == 3) {
                 ShowWinMessage(valueProp)
             }
         }
-        else break;        
+        else break;
     }
 
     if ((valueProp == $("#00").children(0).prop("alt")) &&
         (valueProp == $("#11").children(0).prop("alt")) &&
-        (valueProp == $("#22").children(0).prop("alt")))
-    {
-        ShowWinMessage(valueProp);        
+        (valueProp == $("#22").children(0).prop("alt"))) {
+        ShowWinMessage(valueProp);
     }
 
     if ((valueProp == $("#02").children(0).prop("alt")) &&
         (valueProp == $("#11").children(0).prop("alt")) &&
-        (valueProp == $("#20").children(0).prop("alt")))
-    {
-        ShowWinMessage(valueProp);       
-    }       
+        (valueProp == $("#20").children(0).prop("alt"))) {
+        ShowWinMessage(valueProp);
+    }
 }
 
 //everything is obvious
 function ShowWinMessage(valueProp) {
     alert("Победил " + valueProp);
+    //ViewBag.Winner = valueProp;        
     SetDataInController(valueProp);
     location.reload();
     return;
 }
 
 //sends data to controller
-function SetDataInController(valueProp) {
-    //TODO: set data in BD 
+function SetDataInController(/*valueProp*/) {
+    //$.ajax({
+    //    url:"SupportController.cs",
+    //    type: "POST",
+    //    data: ({ name: $("h3").val })        
+    //});
+
+    var url = "/Home/AddData";
+    $.get(url);
+    //TODO: set data in BD js
 }
